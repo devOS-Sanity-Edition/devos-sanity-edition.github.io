0e9278d17d72b3b1ee2e09446c6bcc06504bcc9e
Devin <devin@hiitsdevin.dev>
2021-06-20 19:55:59 -0700

    Website mostly complete

---
+++ scripts/scroll.js
@@ -0,0 +1,6 @@
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 230) {
+        document.getElementById("firstTitle").innerHTML = "devOS: Sanity Edition";
+        document.getElementById("secondTitle").innerHTML = "A discord server filled with chaotic people and devs alike. Send help.";
+      }
+};
